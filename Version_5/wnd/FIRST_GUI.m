c440b795d06ae945844f798502642faaa3f68219
johnprif <56134761+johnprif@users.noreply.github.com>
2021-11-15 19:35:39 +0200

    Add files via upload

---
+++ Version_5/wnd/FIRST_GUI.m
@@ -0,0 +1,10 @@
+## -*- texinfo -*-
+## @deftypefn  {} {@var{wnd} =} FIRST_GUI ()
+##
+## Create and show the dialog, return a struct as representation of dialog.
+##
+## @end deftypefn
+function wnd = FIRST_GUI()
+  FIRST_GUI_def;
+  wnd = show_FIRST_GUI();
+end
