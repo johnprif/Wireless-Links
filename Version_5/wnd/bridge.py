e8938e6d419706c99411277bc0723d0fad84fd27
johnprif <56134761+johnprif@users.noreply.github.com>
2022-01-17 17:42:09 +0200

    Delete Version_5/wnd directory

--- Version_5/wnd/bridge.py
+++
@@ -1,4 +0,0 @@
-import os
-
-#os.system('python kwstas.py')
-exec(open("kwstas5.py").read())
--- Version_5/wnd/kwstas5.py
+++
@@ -1,7 +0,0 @@
-from maya import cmds
-
-def setImport():
-    setImportPath = 'oldcar.fbx'
-    cmds.file(setImportPath, i=True, mergeNamespacesOnClash=True, namespace=':');
-
-setImport()
