986fa4b8900a1534f8d9f12ee9af3187a12133ce
johnprif <56134761+johnprif@users.noreply.github.com>
2022-01-17 17:45:42 +0200

    Add version_26

---
+++ wnd/FIRST_GUI.m
@@ -0,0 +1,14 @@
+function wnd = FIRST_GUI()
+  pkg load communications;
+  pkg load image;
+  FIRST_GUI_def;
+
+  
+  showReport;
+  wnd = show_FIRST_GUI();
+  #new figure for carSimulator
+  realCarSimulator_figure = figure;
+  #10 = min distance from road
+  realCarSimulator(10, 100, 0);
+  
+end
